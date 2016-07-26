7fac30bf2e7a1b73f3713f0f731419edb23ae900
omar faruk <omarfaruk.sust@gmail.com>
2016-07-26 16:43:29 -0400

    updated

---
+++ footer.php
@@ -0,0 +1,43 @@
+ <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Footer content will goes here</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <script type="text/javascript">
+      $(function() {
+        // We can attach the `fileselect` event to all file inputs on the page
+        $(document).on('change', ':file', function() {
+          var input = $(this),
+              numFiles = input.get(0).files ? input.get(0).files.length : 1,
+              label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+          input.trigger('fileselect', [numFiles, label]);
+        });
+
+        // We can watch for our custom `fileselect` event like this
+        $(document).ready( function() {
+            $(':file').on('fileselect', function(event, numFiles, label) {
+
+                var input = $(this).parents('.input-group').find(':text'),
+                    log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+                if( input.length ) {
+                    input.val(log);
+                } else {
+                    if( log ) alert(log);
+                }
+
+            });
+        });
+        
+      });
+    </script>
+  </body>
+</html>
--- second.php
+++ second.php
@@ -1,50 +1,71 @@
 <?php 
-include "a.php";
-
-$graph = array(
-        0=>array(0,314,268,330,265,232,329,337,309,339),
-        1=>array(314,0,320,385,333,246,292,285,337,229),
-        2=>array(268,320,0,296,236,293,385,313,236,332),
-        3=>array(330,385,296,0,314,327,306,229,237,346),
-        4=>array(265,333,236,314,0,306,341,246,247,322),
-        5=>array(232,246,293,327,306,0,281,365,293,320),
-        6=>array(329,292,385,306,341,281,0,301,308,214),
-        7=>array(337,285,313,229,246,365,301,0,304,274),
-        8=>array(309,337,236,237,247,293,308,304,0,335),
-        9=>array(339,229,332,346,322,320,214,274,335,0),
-    );
+include "prim.php";
 
 // $graph = array(
-//     array(0, 2, 0, 6, 0),
-//     array(2, 0, 3, 8, 5),
-//     array(0, 3, 0, 0, 7),
-//     array(6, 8, 0, 0, 9),
-//     array(0, 5, 7, 9, 0),
-// );
-ini_set("memory_limit",-1);
-$new_graph = array();
+//         0=>array(0,314,268,330,265,232,329,337,309,339),
+//         1=>array(314,0,320,385,333,246,292,285,337,229),
+//         2=>array(268,320,0,296,236,293,385,313,236,332),
+//         3=>array(330,385,296,0,314,327,306,229,237,346),
+//         4=>array(265,333,236,314,0,306,341,246,247,322),
+//         5=>array(232,246,293,327,306,0,281,365,293,320),
+//         6=>array(329,292,385,306,341,281,0,301,308,214),
+//         7=>array(337,285,313,229,246,365,301,0,304,274),
+//         8=>array(309,337,236,237,247,293,308,304,0,335),
+//         9=>array(339,229,332,346,322,320,214,274,335,0),
+//     );
 
-$myfile = fopen("AdjacencyMatrix_of_Graph_G_N_1000.txt", "r") or die("Unable to open file!");
-//echo fread($myfile,filesize("AdjacencyMatrix_of_Graph_G_N_10.txt"));
-$i = 0;
-while(! feof($myfile))
-{
-	//echo fgets($myfile). "<br />";
-	$lines = fgets($myfile); // one line in each time
-	// this is because at the end of the line it's generating empty array
-	if ($lines === false) {
-	    break;
-	 }
-	$splitedArray = preg_split('#\s+#', $lines, null, PREG_SPLIT_NO_EMPTY);
-	$new_graph[$i] = $splitedArray;
-	$i++;
-	
+    if(isset($_POST['btn-upload']))
+    {
+        $uploaddir = __DIR__ . '/uploads';
 
-}
-fclose($myfile);
+        if(isset($_FILES['input_file'])){
+          $errors= array();
+          $file_name = $_FILES['input_file']['name'];
+          $file_size =$_FILES['input_file']['size'];
+          $file_tmp =$_FILES['input_file']['tmp_name'];
+          $file_type=$_FILES['input_file']['type'];
 
-$primsAlgorithm = new PrimsAlgorithm();
-$primsAlgorithm->Prim($new_graph, 1000);
+          // $file_ext=strtolower(end(explode('.',$_FILES['input_file']['name'])));
+          // $expensions= array("txt");
+          // if(in_array($file_ext,$expensions)=== false){
+          //    $errors[]="extension not allowed, please choose a txt file.";
+          // }
+          
+            if (move_uploaded_file($_FILES['input_file']['tmp_name'], "$uploaddir/$file_name")) {
+                ini_set("memory_limit",-1);
+                $new_graph = array();
+                $node_no = $_POST['node_no'];
 
-//$primsAlgorithm->Prim($graph, 5);
+                $input_file = "$uploaddir/$file_name";
+            
+                $myfile = fopen($input_file, "r") or die("Unable to open file!");
+                $i = 0;
+                while(! feof($myfile))
+                {
+                    $lines = fgets($myfile); // one line in each time
+                    // this is because at the end of the line it's generating empty array
+                    if ($lines === false) {
+                        break;
+                     }
+                    $splitedArray = preg_split('#\s+#', $lines, null, PREG_SPLIT_NO_EMPTY);
+                    $new_graph[$i] = $splitedArray;
+                    $i++;
+                }
+                fclose($myfile);
+
+                $primsAlgorithm = new PrimsAlgorithm();
+                $primsAlgorithm->Prim($new_graph, $node_no);
+
+            }else{
+                 die("file is not uploaded.");
+            }
+       } else {
+        die("you did not select any input file");
+       }
+    }
 ?>
+<?php require("header.php"); ?>
+
+    
+
+<?php require("footer.php"); ?>
